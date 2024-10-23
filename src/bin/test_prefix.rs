b1c773a6325a287972a3d271b5f4b6f6598ccb00
Silence <blue_silence@outlook.com>
2024-10-23 06:51:51 -0400

    Prefix Iter test pass.

---
+++ src/bin/test_prefix.rs
@@ -0,0 +1,17 @@
+use rocksdb_rust_binding::wrapper;
+
+
+fn main() {
+    let db = wrapper::DB::open_default("/dev/shm/db_test".to_string());
+    println!("Re:{:?}",db.put(&"123".as_bytes().to_vec(), &"Okk".as_bytes().to_vec()));
+    println!("Re:{:?}",db.get(&"123".as_bytes().to_vec()));
+    println!("Re:{:?}",db.get(&"Empty".as_bytes().to_vec()));
+    for i in 0..1000 {
+        println!("Re:{:?}",db.put(&i.to_string().as_bytes().to_vec(), &(i*10).to_string().as_bytes().to_vec()));
+    }
+
+    let mut iter = wrapper::DbIterator::prefix_iter(&db, &7.to_string().as_bytes().to_vec());
+    for (k,v) in iter {
+        println!("k:{:?}, v:{:?} number:: k:{}, v:{}", k, v, String::from_utf8(k.clone()).unwrap(), String::from_utf8(v.clone()).unwrap());
+    }
+}
